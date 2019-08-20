ef38b85aabc6ce0649e044075d9b4b11d2db057c
zxq <674079013@qq.com>
2019-08-20 10:39:36 +0800

    修改下载地址

--- dc1.sh
+++ dc1.sh
@@ -5,7 +5,7 @@ SERVICE_URL="http://pavt.ponyo.info:10088/file/dc1server.service"
 
 #手动下载文件信息提示
 function echoManualDownloadInfo(){
-    echo -e "\033[31m 文件下载失败，请登陆网页 https://github.com/ZXQ-Kyle/N1Script/Files 手动下载 \033[0m"
+    echo -e "\033[31m 文件下载失败，请登陆网页 https://github.com/ZXQ-Kyle/N1Script/tree/master/Files 手动下载 \033[0m"
     echo -e "\033[31m 手动下载文件并放置到/opt/目录下，再执行该脚本 \033[0m"
     echo -e "\033[31m 共需下载两个文件 dc1server.jar和dc1server.service \033[0m"
 }
@@ -30,7 +30,7 @@ function autoStart(){
     systemctl status dc1server.service
     echo ""
     echoGreenInfo "脚本执行完成，可输入命令 systemctl status dc1server.service 查看状态"
-    echoGreenInfo "手机apk请到 https://github.com/ZXQ-Kyle/N1Script/Files 下载"
+    echoGreenInfo "手机apk请到 https://github.com/ZXQ-Kyle/N1Script/tree/master/Files 下载"
 }
 
 #安装jdk，部署开机启动服务
